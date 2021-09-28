e3ab73eac530cfb066bb73dc8dfa2109e0fed739
Pinheiro-dev <matheus.barbosa@bandtec.com.br>
2021-09-28 00:44:57 -0300

    Componentiza o botão 'Cadastre-se' e ajusta o header

--- Aplicação/Web/React/aplicacao-miaudote/src/App.jsx
+++ Aplicação/Web/React/aplicacao-miaudote/src/App.jsx
@@ -1,12 +1,17 @@
+/* eslint-disable jsx-a11y/alt-text */
 import './css/App.css';
 import background from './imagens/Geral/forma-header.svg';
 import imgLogo from './imagens/Geral/logo-header.svg';
 import imgHome from './imagens/Home/mulher-cachorro-gato-home.svg';
+import imgHeader from './imagens/Geral/forma-header-encolhido.svg'
+import BotaoCadastro from './components/BotaoCadastro';
+import imgOsso from './imagens/Geral/icon-osso.svg'
 import React from 'react'
 
 function App() {
   return (
     <div className="App">
+      <img className="background-header" src={imgHeader} alt="" />
       <div className="container">
         <img className="img-backgound" src={background} />
         <div className="content">
@@ -16,7 +21,7 @@ function App() {
             <img src={imgLogo} />
             <div className="header-botoes">
               <ul className="header-lista">
-                <li>HOME</li>
+                <li className="pagina-atual"> <img src={imgOsso} /> HOME</li>
                 <li>COMO FUNCIONA</li>
                 <li>ADOTE</li>
               </ul> {/* fim hedaer-lista */}
@@ -28,14 +33,14 @@ function App() {
           <div className="container-home">
             <div className="content-home">
               <p className="texto-grande">
-                Encontre sua <br/> pata metade
+                Encontre sua <br /> pata metade
               </p>
               <p className="texto-pequeno">
-                OU AJUDE AS PESSOAS A <br/> ENCONTRAREM O SEU PET IDEAL
+                OU AJUDE AS PESSOAS A <br /> ENCONTRAREM O SEU PET IDEAL
               </p>
-              <button className="botao-cadastrar">Cadastre-se</button>
+              <BotaoCadastro />
             </div> {/* fim content-home */}
-            <img src={imgHome}/>
+            <img src={imgHome} />
           </div> {/* fim container home */}
 
         </div> {/* fim content */}
