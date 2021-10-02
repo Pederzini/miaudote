296598fc437dff3adf253ff5f6a467aa8f417990
bruno-pteixeira <bruno-teixeira@bandtec.com.br>
2021-10-02 18:15:53 -0300

    Adiciona endpoints cadastro ong e adotante

--- Documentos/Banco de Dados/script_criação.sql
+++ Documentos/Banco de Dados/script_criação.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Endereco (
-    idEndereco INT PRIMARY KEY IDENTITY,
+    id_endereco INT PRIMARY KEY IDENTITY,
     logradouro VARCHAR(255),
     numero INT,
     complemento VARCHAR(45),
@@ -8,56 +8,56 @@ CREATE TABLE Endereco (
 );
 
 CREATE TABLE Adotante (
-    idAdotante INT PRIMARY KEY IDENTITY,
+    id_adotante INT PRIMARY KEY IDENTITY,
     nome VARCHAR(60),
-    dataNascimento DATETIME,
+    data_nascimento DATETIME,
     cpf CHAR(11),
     telefone CHAR(11),
     email VARCHAR(255),
     senha VARCHAR(16),
-    urlImagem VARCHAR(2083),
-    fkEndereco INT FOREIGN KEY REFERENCES Endereco(idEndereco)
+    url_imagem VARCHAR(2083),
+    fk_endereco INT FOREIGN KEY REFERENCES Endereco(id_endereco)
 );
 
 CREATE TABLE Ong (
-    idOng INT PRIMARY KEY IDENTITY,
-    nomeResponsavel VARCHAR(60),
-    razaoSocial VARCHAR(45),
-    dataFundacao DATETIME,
+    id_ong INT PRIMARY KEY IDENTITY,
+    nome_responsavel VARCHAR(60),
+    razao_social VARCHAR(45),
+    data_fundacao DATETIME,
     cnpj CHAR(14), 
     telefone CHAR(11),
     email VARCHAR(255),
     senha VARCHAR(16),
-    urlImagem VARCHAR(2083),
-    fkEndereco INT FOREIGN KEY REFERENCES Endereco(idEndereco)
+    url_imagem VARCHAR(2083),
+    fk_endereco INT FOREIGN KEY REFERENCES Endereco(id_endereco)
 );
 
 CREATE TABLE Animal (
-    idAnimal INT PRIMARY KEY IDENTITY,
+    id_animal INT PRIMARY KEY IDENTITY,
     nome VARCHAR(45),
     descricao TEXT,
-    dataNascimento DATETIME,
+    data_nascimento DATETIME,
     genero CHAR(1),
-    dataChegada DATETIME,
-    corPelagem Varchar(20),
+    data_chegada DATETIME,
+    cor_pelagem Varchar(20),
     castrado TINYINT,
     porte VARCHAR(7),
-    tipoPelagem VARCHAR(7),
+    tipo_pelagem VARCHAR(7),
     vacinado TINYINT,
     comportamento VARCHAR(45),
     adotado TINYINT, 
-    necessidadeEspeciais TEXT,
-    urlImagem VARCHAR(2083),
-    fkOng INT FOREIGN KEY REFERENCES Ong(idOng)
+    necessidade_especiais TEXT,
+    url_imagem VARCHAR(2083),
+    fk_ong INT FOREIGN KEY REFERENCES Ong(id_ong)
 
 );
 
 CREATE TABLE ProcessoAdocao (
-    idAdocao INT,
-    dataAdocao DATETIME,
+    id_adocao INT,
+    data_adocao DATETIME,
     favoritado TINYINT,
     feedback TEXT,
-    avaliacaoSite INT,
-    fkAdotante INT FOREIGN KEY REFERENCES Adotante(idAdotante),
-    fkAnimal INT FOREIGN KEY REFERENCES Animal(idAnimal)
+    avaliacao_site INT,
+    fk_adotante INT FOREIGN KEY REFERENCES Adotante(id_adotante),
+    fk_animal INT FOREIGN KEY REFERENCES Animal(id_animal)
 );
