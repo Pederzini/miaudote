cbf2435f8873764fafc5f50a53f5b0ec19de0c39
Pinheiro-dev <matheus.barbosa@bandtec.com.br>
2021-10-02 22:31:10 -0300

    Adiciona ação a função trocaMenu()

--- Aplicação/Web/tela-adocoes/js/adocoes.js
+++ Aplicação/Web/tela-adocoes/js/adocoes.js
@@ -1,7 +1,33 @@
-function trocaMenu() {
+function trocaMenu(valor) {
+    
+    let todasDivs = document.querySelectorAll('.opcoes-adocoes');
 
-    let div = document.querySelector('.opcoes-adocoes')
+    const divs = [document.querySelectorAll('.card-animais-favoritos'),
+                  document.querySelectorAll('.card-processo-adocao'), 
+                  document.querySelectorAll('.card-animais-adotados')]
 
-    div.style.backgroundColor = '#FFD8D5';
+    todasDivs.forEach(element => {
+        let valorDiv = element.getAttribute('value') - 1
+        if (valor == element.getAttribute('value')) {
+            mostrarDivs(divs[valorDiv])
+            element.style.backgroundColor = '#FFD8D5';
+        } else {
+            let div = divs[valorDiv]
+            bloquearDivs(div)
+            element.style.backgroundColor = '#FFFFFF';
+        }
+    });
+
+    function bloquearDivs(div) {
+        div.forEach(element => {
+            element.style.display = 'none';
+        });
+    }
+
+    function mostrarDivs(div) {
+        div.forEach(element => {
+            element.style.display = 'block';
+        });
+    }
 
 }
