87980941c0ef70eb4cd0f2f7ae236edad97027b1
Pinheiro-dev <matheus.barbosa@bandtec.com.br>
2021-10-09 15:44:57 -0300

    Cria div 'Animais Favoritados' pelo javascript

--- Aplicação/Web/tela-adocoes/js/adocoes.js
+++ Aplicação/Web/tela-adocoes/js/adocoes.js
@@ -1,59 +1,37 @@
-// function trocaMenu(valor) {
+function trocaMenu(valor) {
     
-//     let todasDivs = document.querySelectorAll('.opcoes-adocoes');
+    let todasDivs = document.querySelectorAll('.opcoes-adocoes');
 
-//     const divs = [document.querySelectorAll('.card-animais-favoritos'),
-//                   document.querySelectorAll('.card-processo-adocao'), 
-//                   document.querySelectorAll('.card-animais-adotados')]
+    const divs = [document.querySelectorAll('.card-animais-favoritos'),
+                  document.querySelectorAll('.card-processo-adocao'), 
+                  document.querySelectorAll('.card-animais-adotados')]
 
-//     todasDivs.forEach(element => {
-//         let valorDiv = element.getAttribute('value') - 1
-//         if (valor == element.getAttribute('value')) {
-//             mostrarDivs(divs[valorDiv])
-//             element.style.backgroundColor = '#FFD8D5';
-//         } else {
-//             let div = divs[valorDiv]
-//             bloquearDivs(div)
-//             element.style.backgroundColor = '#FFFFFF';
-//         }
-//     });
+    todasDivs.forEach(element => {
+        let valorDiv = element.getAttribute('value') - 1
+        if (valor == element.getAttribute('value')) {
+            mostrarDivs(valor)
+            element.style.backgroundColor = '#FFD8D5';
+        } else {
+            let div = divs[valorDiv]
+            bloquearDivs(div)
+            element.style.backgroundColor = '#FFFFFF';
+        }
+    });
 
-//     function bloquearDivs(div) {
-//         div.forEach(element => {
-//             element.style.display = 'none';
-//         });
-//     }
+    function bloquearDivs(div) {
+        div.forEach(element => {
+            element.style.display = 'none';
+        });
+    }
 
-//     function mostrarDivs(div) {
-//         div.forEach(element => {
-//             element.style.display = 'block';
-//         });
-//     }
+}
 
-// }
-
-function trocarMenu(valor) {
-
-    // if (ramIDE > 2) {
-    //     let alerta = document.createElement('div')
-    //     alerta.innerHTML = "1"
-    //     first.appendChild(alerta);
-    //     if (alerta.classList) alerta.classList.add("alert");
-    //     else alerta.className += " alert";
-    //   }
-
-    //   let img1 = document.createElement('img')
-    //   img1.src = `../../img/${nomeIDE}.png`
-    //   img1.id = nomeIDE
-    //   img1.addEventListener('click', function () {
-    //     sessionStorage.ide_usuario_meu_app = nomeIDE
-    //     window.location.href = 'graficoshardware.html'
-    //   })
-    let divCardAdocoes = document.querySelector(".cards-adocoes")
+function mostrarDivs(valor) {
+    let div = document.querySelector(".cards-adocoes")
 
     if (valor == 1) {
         let divAnimaisFavoritos = document.createElement('div')
-        divCardAdocoes.appendChild(divAnimaisFavoritos)
+        div.appendChild(divAnimaisFavoritos)
         if (divAnimaisFavoritos.classList) divAnimaisFavoritos.classList.add("card-animais-favoritos");
         else divAnimaisFavoritos.className += " card-animais-favoritos";
 
@@ -69,23 +47,86 @@ function trocarMenu(valor) {
         else divImgPet.className += " img-pet";
 
         let imgPet = document.createElement('img')
-        divImgPet.appendChild(imgPet)
-        // talves precise passar o SRC por id
         imgPet.src = "../../imagens/Pets-teste/cachorro.svg"
+        divImgPet.appendChild(imgPet)
 
         let divTextoPet = document.createElement('div')
         divDadosPet.appendChild(divTextoPet)
         if (divTextoPet.classList) divTextoPet.classList.add("texto-pet");
         else divTextoPet.className += " texto-pet";
 
+        let pThor = document.createElement('p')
+        pThor.innerHTML = "THOR" 
+        divTextoPet.appendChild(pThor)
+       
+        let pAnos = document.createElement('p')
+        pAnos.innerHTML = "3 ANOS"
+        divTextoPet.appendChild(pAnos)  
+
+        let divSexoPet = document.createElement('div')
+        divTextoPet.appendChild(divSexoPet)
+        if (divSexoPet.classList) divSexoPet.classList.add("sexo-pet");
+        else divSexoPet.className += " sexo-pet";
+
+        let pSexo = document.createElement('p')
+        pSexo.innerHTML = "MACHO" 
+        divSexoPet.appendChild(pSexo)
+
+        let imgSexo = document.createElement('img')
+        imgSexo.src = "../../imagens/Adocoes/sexo-masculino.svg"
+        divSexoPet.appendChild(imgSexo)
+       
         // DIV DADOS FAVORITOS E OQ ESTÁ DENTRO DELA
         let divDadosFavoritos = document.createElement('div')
         divAnimaisFavoritos.appendChild(divDadosFavoritos)
         if (divDadosFavoritos.classList) divDadosFavoritos.classList.add("dados-favoritos");
         else divDadosFavoritos.className += " dados-favoritos";
 
+        let divImgFavorito = document.createElement('div')
+        divDadosFavoritos.appendChild(divImgFavorito)
+        if (divImgFavorito.classList) divImgFavorito.classList.add("img-favorito");
+        else divImgFavorito.className += " img-favorito";
 
+        let containerFavorito = document.createElement('div')
+        divImgFavorito.appendChild(containerFavorito)
+        if (containerFavorito.classList) containerFavorito.classList.add("container-favorito");
+        else containerFavorito.className += " container-favorito";
 
+        let imgCoracaoVermelho = document.createElement('img')
+        imgCoracaoVermelho.src = "../../imagens/Geral/icon-coracao-vermelho.svg"
+        containerFavorito.appendChild(imgCoracaoVermelho)
+
+        let caontadorFavorito = document.createElement('div')
+        divDadosFavoritos.appendChild(caontadorFavorito)
+        if (caontadorFavorito.classList) caontadorFavorito.classList.add("contador-favvorito");
+        else caontadorFavorito.className += " contador-favvorito";
+
+        let cliqueFavorito = document.createElement('div')
+        divDadosFavoritos.appendChild(cliqueFavorito)
+        if (cliqueFavorito.classList) cliqueFavorito.classList.add("clique-favorito");
+        else cliqueFavorito.className += " clique-favorito";
+
+        let containerClique = document.createElement('div')
+        cliqueFavorito.appendChild(containerClique)
+        if (containerClique.classList) containerClique.classList.add("container-clique");
+        else containerClique.className += " container-clique";
+
+        let containerAlinhamento = document.createElement('div')
+        containerClique.appendChild(containerAlinhamento)
+        if (containerAlinhamento.classList) containerAlinhamento.classList.add("container-alinhamento");
+        else containerAlinhamento.className += " container-alinhamento";
+
+        let pAlinhamento = document.createElement('p')
+        pAlinhamento.innerHTML = "CLIQUE AQUI PRA VER QUEM FAVORITOU O THOR"
+        containerAlinhamento.appendChild(pAlinhamento)
+
+    } else if (valor == 2) {
+        let div = document.querySelector(".cards-adocoes")
+
+        let divDadosFavoritos = document.createElement('div')
+        divAnimaisFavoritos.appendChild(divDadosFavoritos)
+        if (divDadosFavoritos.classList) divDadosFavoritos.classList.add("dados-favoritos");
+        else divDadosFavoritos.className += " dados-favoritos";
     }
 }
 
