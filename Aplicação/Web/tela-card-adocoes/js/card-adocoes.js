4ca18aabd20f55877b79e75c85495c8c0ea27de5
PiresIsabella <isabella.silva@valemobi.com.br>
2021-10-12 00:51:25 -0300

    Adiciona tela de card de adoções

---
+++ Aplicação/Web/tela-card-adocoes/js/card-adocoes.js
@@ -0,0 +1,12 @@
+const
+	range = document.getElementById('range'),
+	rangeV = document.getElementById('rangeV'),
+	setValue = ()=>{
+		const
+			newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) ),
+			newPosition = 10 - (newValue * 0.2);
+		rangeV.innerHTML = `<span>${range.value}</span>`;
+		rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
+	};
+document.addEventListener("DOMContentLoaded", setValue);
+range.addEventListener('input', setValue);
