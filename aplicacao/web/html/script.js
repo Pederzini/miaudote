c7fe534f98542b3d5adeb20a6b9fca58e0863b3a
Shoyo <shoyo@hotmail.com>
2021-10-17 16:28:37 -0300

    Corrige onload body

--- aplicacao/web/html/script.js
+++ aplicacao/web/html/script.js
@@ -6,19 +6,18 @@ function redirecionar_login() {
 }
 
 function validarSessao() {
-
-    // login_usuario = localStorage.getItem('login_usuario');
-    login_usuario = sessionStorage.login_usuario
+    login_usuario = sessionStorage.login_usuario;
 
     if (login_usuario == undefined) {
         logoff();
     } else {
-
-        // se no storage tiver CNPJ, mostrar nomeResponsavel, senao mosstrar nome
-        let nome = document.getElementById("nome")
-
-        nome.innerHTML = `${JSON.parse(login_usuario).nomeResponsavel}`
-        console.log(JSON.parse(login_usuario).nomeResponsavel)    
+        let nomeResponsa = JSON.parse(login_usuario).nomeResponsavel;
+        let nome = document.getElementById("nome");
+        if (nomeResponsa == undefined) {
+            nome.innerHTML = `${JSON.parse(login_usuario).nome}`;
+        } else {
+            nome.innerHTML = `${JSON.parse(login_usuario).nomeResponsavel}`;  
+        }        
     } 
 }
 
