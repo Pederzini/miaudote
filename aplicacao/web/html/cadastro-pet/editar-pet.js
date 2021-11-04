bf372f0bd0ceb17e346aad84af0fa7cb82a68e02
Shoyo <shoyo@hotmail.com>
2021-11-04 20:54:16 -0300

    Cria pagina editar animal

---
+++ aplicacao/web/html/cadastro-pet/editar-pet.js
@@ -0,0 +1,8 @@
+var foto1 = "../../imagens/geral/placeholder-imagem-pet.svg";
+
+function trocaImagem() {
+
+  if (fotoBanco == null) {
+    return foto = "../../imagens/geral/placeholder-imagem-pet.svg";
+  }
+}
