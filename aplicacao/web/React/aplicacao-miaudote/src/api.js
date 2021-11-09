96cdb2efe0ff61b14b9b39ec88d80a9747afbed4
Pinheiro-dev <matheus.barbosa@bandtec.com.br>
2021-11-09 00:19:08 -0300

    Ajusta chamada da api

--- aplicacao/web/React/aplicacao-miaudote/src/api.js
+++ aplicacao/web/React/aplicacao-miaudote/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "localhost:8080/miaudote/animal/vitrine"
+  baseURL: "http://localhost:8080/miaudote/animais/vitrine"
 });
 
 export default api;
