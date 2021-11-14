f9fe0cea81af0263667c043838bf29f328ea074b
Pederzini <victor.pederzini@bandtec.com.br>
2021-11-14 15:00:11 -0300

    Refatora método Adapter para ser static

--- aplicacao/backend/miaudote-api/src/main/java/br/com/miaudote/miaudoteapi/utilitarios/GoogleAdapter.java
+++ aplicacao/backend/miaudote-api/src/main/java/br/com/miaudote/miaudoteapi/utilitarios/GoogleAdapter.java
@@ -1,15 +1,15 @@
 package br.com.miaudote.miaudoteapi.utilitarios;
 
+import br.com.miaudote.miaudoteapi.dominio.Endereco;
 import br.com.miaudote.miaudoteapi.dominio.Usuario;
 import org.json.JSONArray;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.client.RestTemplate;
 import org.json.JSONObject;
 
-
 public class GoogleAdapter {
-    public Usuario registrarLatAndLong(Usuario usuario){
 
+    public static Usuario registrarLatAndLong(Usuario usuario) {
         RestTemplate restTemplate = new RestTemplate();
         RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
 
@@ -18,7 +18,7 @@ public class GoogleAdapter {
         restTemplate = restTemplateBuilder.build();
         String dadosStr = restTemplate.getForObject(String.format("https://maps.google.com/maps/api/geocode/" +
                 "json?address=%s&components=country:BR" +
-                "&key=AIzaSyBW_ZLgy04TjWvV9i_5HaGXG1ypQ7lsMDs", endereco),String.class );
+                "&key=AIzaSyBW_ZLgy04TjWvV9i_5HaGXG1ypQ7lsMDs", endereco), String.class);
         JSONObject dados = new JSONObject(dadosStr);
         JSONArray array = dados.getJSONArray("results");
         JSONObject addressComponents = array.getJSONObject(0);
@@ -30,6 +30,30 @@ public class GoogleAdapter {
         usuario.getEndereco().setLatitude(latitude);
         usuario.getEndereco().setLongitude(longitude);
         return usuario;
+    }
+
+    public static Endereco buscarLatAndLong(Endereco endereco) {
+        RestTemplate restTemplate = new RestTemplate();
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+
+        String enderecoFormatado = endereco.getEnderecoFormatado();
+
+        restTemplate = restTemplateBuilder.build();
+        String dadosStr = restTemplate.getForObject(String.format("https://maps.google.com/maps/api/geocode/" +
+                "json?address=%s&components=country:BR" +
+                "&key=AIzaSyBW_ZLgy04TjWvV9i_5HaGXG1ypQ7lsMDs", enderecoFormatado), String.class);
+        JSONObject dados = new JSONObject(dadosStr);
+        JSONArray array = dados.getJSONArray("results");
+        JSONObject addressComponents = array.getJSONObject(0);
+        JSONObject geometry = addressComponents.getJSONObject("geometry");
+        JSONObject location = geometry.getJSONObject("location");
+        Double latitude = location.getDouble("lat");
+        Double longitude = location.getDouble("lng");
+
+        endereco.setLatitude(latitude);
+        endereco.setLongitude(longitude);
+
+        return endereco;
     }
 
 }
