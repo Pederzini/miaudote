017a8a6612d26f8d6cf1c1c3f5d9713f07ce67c5
Pinheiro-dev <matheus.barbosa@bandtec.com.br>
2021-11-20 22:41:26 -0300

    Altera tela de perfil Ong

--- aplicacao/web/html/tela-perfil-pet-ong/perfil-pet-ong.js
+++ aplicacao/web/html/tela-perfil-pet-ong/perfil-pet-ong.js
@@ -2,24 +2,21 @@ function getInfosPet() {
     axios.get(`http://localhost:8080/miaudote/animais/${id}`, {
         headers: { "Access-Control-Allow-Origin": "*", "crossorigin": true },
     }).then(response => {
-        document.getElementById("campo_razao").value = response.data.razaoSocial;
-        document.getElementById("campo_cnpj_escondido").value = formataCnpj(response.data.cnpj);
-        document.getElementById("campo_cnpj").value = formataCnpjAsterisco(response.data.cnpj);
-        document.getElementById("campo_fundacao").value = formataData(response.data.dataFundacao);
-        document.getElementById("campo_resp").value = response.data.nomeResponsavel;
-        document.getElementById("campo_telefone").value = formataTelefone(response.data.telefone);
-        document.getElementById("campo_cep").value = formataCep((response.data.endereco.cep).trim());
-        document.getElementById("campo_logradouro").value = response.data.endereco.logradouro;
-        document.getElementById("campo_bairro").value = response.data.endereco.bairro;
-        document.getElementById("campo_numero").value = response.data.endereco.numero;
-        document.getElementById("campo_complemento").value = response.data.endereco.complemento;
-        document.getElementById("campo_email").value = response.data.email;
-        document.getElementById("campo_senha").value = response.data.senha;
-        document.getElementById("campo_confirmar_senha").value = response.data.senha;
-        if (response.data.urlImagem.length != 0) {
-            document.getElementById("imagePerfil").src = response.data.urlImagem;
-            document.getElementById("textoUploader").innerHTML = "";
-        }
+        document.getElementById("campo_idade").value = response.data.razaoSocial;
+        document.getElementById("campo_porte").value = formataCnpj(response.data.cnpj);
+        document.getElementById("campo_chegada").value = formataCnpjAsterisco(response.data.cnpj);
+        document.getElementById("campo_castrado").value = formataData(response.data.dataFundacao);
+        document.getElementById("campo_sexo").value = response.data.nomeResponsavel;
+        document.getElementById("campo_pelagem").value = formataTelefone(response.data.telefone);
+        document.getElementById("campo_comportamento").value = formataCep((response.data.endereco.cep).trim());
+        document.getElementById("campo_vacinacao").value = response.data.endereco.logradouro;
+        document.getElementById("campo_necessidades").value = response.data.endereco.bairro;
+        document.getElementById("campo_descricao").value = response.data.endereco.numero;
+
+        // if (response.data.urlImagem.length != 0) {
+        //     document.getElementById("imagePerfil").src = response.data.urlImagem;
+        //     document.getElementById("textoUploader").innerHTML = "";
+        // }
     }).catch(function (error) {
         Swal.fire({
             title: error.response,
@@ -29,3 +26,4 @@ function getInfosPet() {
         })
     })
 }
+
