101d8eaba6fc0b76b309e334259a7b427c139452
PiresIsabella <isabella.silva@bandtec.com.br>
2022-04-08 13:54:34 -0300

    Refatora branch

---
+++ aplicacao/Miaudote/Aplicação/Backend/miaudote/src/test/java/br/com/miaudote/miaudote/MiaudoteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miaudote.miaudote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaudoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ aplicacao/Miaudote/app/src/main/java/com/miaudote/PerfilPetAdotanteActivity.kt
@@ -0,0 +1,13 @@
+package com.miaudote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PerfilPetAdotanteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_perfil_pet_adopter)
+
+    }
+}
